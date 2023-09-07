b400764eec187070434bc3996ed2e191eb7200b7
NamalSanjaya <namalsanjaya23@gmail.com>
2023-09-07 12:10:20 +0530

    Remove ~ directory change

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,22 +12,21 @@ sudo yum install -y mongodb-org
 sudo systemctl start mongod
 sudo sed -i 's/^ *bindIp: .*/  bindIp: 0.0.0.0/' /etc/mongod.conf
 sudo systemctl restart mongod
-
+cd /home/ec2-user
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.nvm/nvm.sh
+. /home/ec2-user/.nvm/nvm.sh
 nvm install --lts
 sudo dnf install git -y
-cd ~
-mkdir logs
+mkdir /home/ec2-user/logs
 git clone https://github.com/NamalSanjaya/todo-app.git
-cd ~/todo-app/todo-runtime
+cd /home/ec2-user/todo-app/todo-runtime
 npm install
-nohup npm run start:dev > ~/logs/nest_server.log &
+nohup npm run start:dev > /home/ec2-user/logs/nest_server.log &
 echo "nest server started....8000"
-cd ~/todo-app/todo-frontend
+cd /home/ec2-user/todo-app/todo-frontend
 npm install
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 PUBLIC_IP=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/public-ipv4`
-nohup env REACT_APP_HOST_DOMAIN=$PUBLIC_IP npm start > ~/logs/react_app.log &
+nohup env REACT_APP_HOST_DOMAIN=$PUBLIC_IP npm start > /home/ec2-user/logs/react_app.log &
 echo "react server started....3000"
 echo "complete system deployed successfully."
